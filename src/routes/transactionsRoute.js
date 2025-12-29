581e82bb08c4856da9c1316358d4bb25d1391922
AbhishekGarg9644 <164864124+AbhishekGarg9644@users.noreply.github.com>
2025-12-29 21:18:37 +0530

    minor fix

--- src/routes/transactionsRoute.js
+++ src/routes/transactionsRoute.js
@@ -1,21 +1,17 @@
-import express from "express"
+import express from "express";
 
-import {getTransactionByUserId,
-        createTransaction,
-         deleteTransaction,
-        getSummaryByUserId } from "../controllers/transactionsController.js"
+import {
+  getTransactionByUserId,
+  createTransaction,
+  deleteTransaction,
+  getSummaryByUserId,
+} from "../controllers/transactionsController.js";
 
 const router = express.Router();
-router.get("/summary/:userId" , getSummaryByUserId); 
 
-
-router.get("/:userId",getTransactionByUserId);
+router.get("/summary/:userId", getSummaryByUserId);
+router.get("/:userId", getTransactionByUserId);
 router.post("/", createTransaction);
 router.delete("/:id", deleteTransaction);
 
-
- 
-
-
 export default router;
- 
